11eb3fcbdbbb63b3d5503ff63c3ec23b812f3e8b
Shelton Bilgates <sheltonbilgates@gmail.com>
2024-01-29 22:31:04 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SpellCheck from './Component/SpellCheck'
+
+const App = () => {
+  return (
+    <div>
+      <SpellCheck />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/SpellCheck.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const SpellCheck = () => {
+    const [inputText, setInputText] = useState("")
+    const [suggestedText, setSuggestedText] = useState("")
+
+
+  const customDictionary = {
+    teh: "the",
+
+    wrok: "work",
+
+    fot: "for",
+
+    exampl: "example",
+  };
+
+  const handleChange = (e) =>{
+    const text = e.target.value
+    setInputText(text)
+
+    const words = text.split(" ")
+    const correctedWords = words.map((word) => {
+        const correctedWord = customDictionary[word.toLowerCase()];
+        return correctedWord || word;
+      });
+      const correctedText = correctedWords.join(" ")
+
+      const firstCorrection = correctedWords.find((word, index) => word !== words[index])
+      setSuggestedText(firstCorrection || "")
+  }
+
+  return (
+    <div>
+      <h1>Spell Check and Auto-Correction</h1>
+      <textarea value={inputText} cols="40" rows="5" placeholder="Enter text..." onChange={(e) => handleChange(e)}></textarea>
+      {suggestedText && (
+        <p>Did you mean: <strong>{suggestedText}</strong>?</p>
+      )}
+    </div>
+  );
+};
+
+export default SpellCheck;
